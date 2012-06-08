e1b11d07a307cd64e649abfd6843c77c36aca0cb
d34th <ubpd34th@gmail.com>
2012-06-08 00:33:46 -0400

    Fixed some php
    dont break it this time greedi

--- submitted.php
+++ submitted.php
@@ -1,44 +1,36 @@
 <?php
-error_reporting(-1);
 include("core/wallet.php");
 include('templates/header.php');
-
-// Submitted page
-?>
+echo ' <html>
         <div class="page-header">
           <h1>Daily Litecoin <small>In progress...</small></h1>
         </div>
         <div class="row">
           <div class="span10">
           
-<center>
-<?php
-if(isset($_POST['name'])) { $name = $_POST['name']; }
+<center>';
+if(isset($_POST['name'])) { $name = $_POST['name']; } else {header("location: daily.php");}
 $ip = $_SERVER['REMOTE_ADDR'];
-if(!$name){header("location: daily.php");}
-else
-{
- $a = fopen("addresses/$ip.txt","a+");
- fwrite($a,"-------------------------\n");
- fwrite($a,$ip."\n");
- fwrite($a,$name."\n");
- fclose($a);
- ?>
- <html><br>
+if(isset($name)) {
+	$fname = "addresses/$ip.txt";
+	if(is_writable($fname)){
+	if(!$fp = fopen($fname,"a+")) { $error_step = "Unable To Open $fname"; }
+	if(!fwrite($fp,"-------------------------\n$ip\n$name\n")){;$error_step = "unable to write to $fname"}
+	if(!fclose($fp)){$error_step = "Unable to close $fname check if opened";}
+	echo '
+<br>
  <h1>Submitted!</h1><br>
  You will get your LTC in the end of this round !
  </html>
- <?
- echo "</table>";
- fclose($a);
- }
-?>
+</table>
+
 <form action="index">
 <input type="submit" value="Back">
 </center></form>
-</div>
-<?
-include("templates/sidebar.php");
+</div>';
+} else {$error_step = "unable to open $fname";} }
+else { echo $error_step; }
+include('templates/sidebar.php');
 include('templates/footer.php');
 ?>
 
